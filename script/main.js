2973cb4d4ea92e72ca107ab5c21982adf80e3dca
Pammachius Jérôme <scipions.prof@free.fr>
2023-11-29 08:50:07 +0100

    exo fini

--- script/main.js
+++ script/main.js
@@ -7,10 +7,19 @@ let reponse = document.getElementById("reponse");
 
 document.getElementById("bouton").onclick = () => {
     heure = document.querySelector('input[id="heure"]').value;
-    // console.log(heure);
     minute = document.querySelector('input[id="minute"]').value;
-    // console.log(minute);
-    // peut se faire avec id
     seconde = document.getElementById("seconde").value;
-    console.log(seconde);
+    seconde++;
+    if (seconde === 60) {
+        minute++;
+        seconde = 0;
+    }
+    if (minute === 60) {
+        heure++;
+        minute = 0;
+    } 
+    if (heure === 24) {
+        heure = 0;
+    }
+    reponse.innerHTML = heure + " heure " + minute + " minute " + seconde + " seconde";
 }
